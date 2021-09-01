8a2792d6b17e2e20a3e10414a2a28cefba5a3caf
NgoXiem <89287175+NgoXiem@users.noreply.github.com>
2021-09-01 16:11:48 +0700

    Add files via upload

---
+++ 06. DOM navbar/app.js
@@ -0,0 +1,11 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+const btn = document.querySelector('button');
+const links = document.querySelector('.links');
+
+btn.addEventListener('click', () => {
+    links.classList.toggle('show-links');
+})
